529bae19263f2c2434b8b6be87d252eba4b86155
Duvis07 <duvan.botero@est.iudigital.edu.co>
2022-11-04 11:36:38 -0500

    rutas publicas y ultimo inicio de sesion

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -8,8 +8,10 @@ export const LoginPage = () => {
   const navigate = useNavigate();
 
   const onLogin = () => {
+    const lastPath = localStorage.getItem("lastPath") || "/";
+
     login("Duvan");
-    navigate("/", { replace: true });
+    navigate(lastPath, { replace: true });
   };
 
   return (
