cd3b34539770278e5298194d4c0d05b60612bace
Duvis07 <duvan.botero@est.iudigital.edu.co>
2022-11-04 10:39:25 -0500

    Se implementa rutas privadas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
+
   const navigate = useNavigate();
 
   const onLogin = () => {
+    login("Duvan");
     navigate("/", { replace: true });
   };
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
@@ -10,7 +11,16 @@ export const AppRouter = () => {
       <Routes>
         <Route path="login" element={<LoginPage />} />
 
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+
+        {/*    <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   );
